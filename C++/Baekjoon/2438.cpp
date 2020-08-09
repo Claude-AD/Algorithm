8ffc0f34a0f564015a48bc71fa4f8891dc5d36dc
Junhyeok Yun <claudead777@gmail.com>
2020-08-09 16:49:09 +0900

    별 찍기 - 1

--- C++/Baekjoon/2438.cpp
+++ C++/Baekjoon/2438.cpp
@@ -3,16 +3,16 @@ using namespace std;
 
 int main()
 {
-	int num;
-	cin >> num;
+	int num; // 몇 번째 줄까지 별을 출력할 것인지
+	cin >> num; // 사용자로부터 입력받음
 
-	for (int i = 1; i <= num; i++)
+	for (int i = 1; i <= num; i++) // 지금 몇 번째 줄인지 나타냄
 	{
-		for (int j = 0; j < i; j++)
+		for (int j = 0; j < i; j++) // 지금 N번째 줄이라면 별 N개를 찍음
 		{
 			cout << '*';
 		}
-		cout << '\n';
+		cout << '\n'; // 줄이 끝날 때 마다 개행문자 출력
 	}
 	return 0;
 }
