7e863ae32463408555fc9ac8ea10229a304f2b2d
Junhyeok Yun <claudead777@gmail.com>
2020-08-10 22:10:54 +0900

    그릇

--- C++/Baekjoon/7567.cpp
+++ C++/Baekjoon/7567.cpp
@@ -4,27 +4,25 @@ using namespace std;
 
 int main()
 {
-	string plates;
-	int plates_height = 0;
+	string plates; // 사용자로부터 입력받을 그릇 문자열
+	int plates_height = 0; // 그릇의 최종 높이
 
-	do {
-		cin >> plates;
-	} while (getc(stdin) == ' ');
+	cin >> plates; // 사용자로부터 그릇 문자열 입력받음
 
-	for (int i = 0; i < plates.length(); i++)
+	for (int i = 0; i < plates.length(); i++) // 총 그릇의 개수만큼 반복
 	{
 		if (i == 0)
-			plates_height += 10;
+			plates_height += 10; // 처음 바닥에 있는 그릇의 높이는 10cm
 
 		else
 		{
 
-			if (plates[i - 1] == plates[i])
+			if (plates[i - 1] == plates[i]) // 같은 방향으로 포개면 높이 5cm 증가
 				plates_height += 5;
-			else
+			else // 반대방향으로 쌓이면 높이 10cm 증가
 				plates_height += 10;
 		}
 	}
-	cout << plates_height << endl;
+	cout << plates_height << endl; // 그릇의 최종 높이 출력
 	return 0;
 }
