37297e0241bc16dae0bd42a4539c20edfaa8aff3
Junhyeok Yun <claudead777@gmail.com>
2020-08-10 21:06:13 +0900

    피보나치 수

--- C++/Baekjoon/2747.cpp
+++ C++/Baekjoon/2747.cpp
@@ -5,26 +5,32 @@ using namespace std;
 
 int main()
 {
-	int num;
-	cin >> num;
+	int num; // 몇 번째 피보나치 수를 찾을 것인지
+	cin >> num; // 사용자로부터 입력받음
 
-	if (num == 0 || num == 1)
+	if (num == 0 || num == 1) // 0이나 1을 입력받으면
 	{
-		cout << num << endl;
+		cout << num << endl; // 각각 0과 1을 출력
 	}
 
 	else
 	{
-		int zero = 0;
-		int one = 1;
-		int fibo = 0;
-		for (int i = 1; i < num; i++)
+		int zero = 0; // n - 2번째 피보나치 수
+		int one = 1; // n - 1번째 피보나치 수
+		int fibo = 0; // n번째 피보나치 수
+
+		/*
+		사용자로부터 입력받은 수가 N이라고 했을 때 N번째 피보나치 수를 구하기 위해
+		아래 반복문을 N-1번 반복한다.
+		*/
+
+		for (int i = 1; i < num; i++) 
 		{
-			fibo = zero + one;
-			zero = one;
-			one = fibo;
+			fibo = zero + one; // n - 2번째 피보나치 수와 n - 1번째 피보나치 수를 더해서 n번째 피보나치 수를 구함
+			zero = one; // n - 1번째 피보나치 수를 n - 2번째 피보나치 수로 변경
+			one = fibo; // n번째 피보나치 수를 n - 1번째 피보나치 수로 변경
 		}
-		cout << fibo << endl;
+		cout << fibo << endl; // N번째 피보나치 수 출력
 	}
 	return 0;
 }
