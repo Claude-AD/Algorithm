f8e61675985a0bc0dd0fffb2abaff903a3f05c6e
Junhyeok Yun <claudead777@gmail.com>
2020-08-10 22:14:41 +0900

    더하기

--- C++/Baekjoon/9085.cpp
+++ C++/Baekjoon/9085.cpp
@@ -3,22 +3,22 @@ using namespace std;
 
 int main()
 {
-	int test_case;
-	cin >> test_case;
+	int test_case; // 테스트 케이스의 개수
+	cin >> test_case; // 사용자로부터 입력받음
 
-	for (int i = 0; i < test_case; i++)
+	for (int i = 0; i < test_case; i++) // 테스트 케이스의 수만큼 반복
 	{
-		int sum = 0;
-		int num;
-		cin >> num;
+		int sum = 0; // 자연수의 합
+		int num; // 자연수의 개수(N)
+		cin >> num; // N을 사용자로부터 입력받음
 
-		for (int i = 0; i < num; i++)
+		for (int i = 0; i < num; i++) // N번 반복
 		{
-			int natural_number;
-			cin >> natural_number;
-			sum += natural_number;
+			int natural_number; // 자연수
+			cin >> natural_number; // 자연수를 사용자로부터 입력받음
+			sum += natural_number; // 입력받은 자연수를 sum에다가 더해줌 
 		}
-		cout << sum << endl;
+		cout << sum << endl; // 입력받은 N개 자연수의 총합 출력
 	}
 	return 0;
 }
