0a46522ddd843e20a9d34213e96b34118477a7e1
Junhyeok Yun <claudead777@gmail.com>
2020-08-10 22:21:35 +0900

    Merciful Algospot

--- C++/Baekjoon/Mercy.cpp
+++ C++/Baekjoon/Mercy.cpp
@@ -3,10 +3,10 @@ using namespace std;
 
 int main()
 {
-	int num;
-	cin >> num;
+	int num; 
+	cin >> num; // 사용자로부터 정수를 입력받음
 
-	for (int i = 0; i < num; i++) 
+	for (int i = 0; i < num; i++) // 그 정수만큼 Hello Algospot!을 출력
 	{
 		cout << "Hello Algospot!\n";
 	}
