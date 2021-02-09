67ed3fe83492e9c3a3715918944cd03a02adc814
Junhyeok Yun <claudead777@gmail.com>
2021-02-09 16:39:28 +0900

    Update Ch.2-13.cpp

--- C++/명품 C++ programming/Chapter 2/Ch.2-13.cpp
+++ C++/명품 C++ programming/Chapter 2/Ch.2-13.cpp
@@ -1,39 +1,48 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-int main()
-{
-	cout << "***** 승리장에 오신 것을 환영합니다. *****" << endl;
-	
-	while (true)
-	{
-		int order;
-		cout << "짬뽕:1, 짜장:2, 군만두:3, 종료:4>> ";
-		cin >> order;
+void order(string menu);
 
-		string menu;
-		if (order == 1)
-			menu = "짬뽕";
-		else if (order == 2)
-			menu = "짜장";
-		else if (order == 3)
-			menu = "군만두";
-		else if (order == 4)
-		{
-			cout << "오늘 영업은 끝났습니다." << endl;
-			break;
-		}
-		else
-		{
-			cout << "다시 주문하세요!!\n";
-			continue;
-		}
+int main() {
+    cout << "***** 승리장에 오신 것을 환영합니다. *****\n";
+    while(1) {
+        int menu;
+        cout << "짬뽕:1, 짜장:2, 군만두:3, 종료:4>>\t";
+        cin >> menu;
+        
+        switch (menu) {
+            case 1:
+                order("짬뽕");
+                break;
+                
+            case 2:
+                order("짜장");
+                break;
+                
+            case 3:
+                order("군만두");
+                break;
+                
+            case 4:
+                cout << "오늘 영업은 끝났습니다.\n";
+                break;
+                
+            default:
+                cout << "다시 주문하세요!!\n";
+                break;
+        }
+        if (menu == 4) {
+            break;
+        }
+    }
+    return 0;
+}
 
-		int people = 0;
-		cout << "몇인분?";
-		cin >> people;
-
-		printf("%s %d인분 나왔습니다\n", menu.c_str(), people);
-	}
-	return 0;
+void order(string menu) {
+    int people;
+    cout << "몇인분?";
+    cin >> people;
+    
+    cout << menu + " " << people << "인분 나왔습니다\n";
 }
