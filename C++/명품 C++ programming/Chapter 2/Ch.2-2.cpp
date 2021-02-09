5ce17c705e84453be97f07786afdc5653cb55530
Junhyeok Yun <claudead777@gmail.com>
2021-02-09 09:47:51 +0900

    Update Ch.2-2.cpp

--- C++/명품 C++ programming/Chapter 2/Ch.2-2.cpp
+++ C++/명품 C++ programming/Chapter 2/Ch.2-2.cpp
@@ -1,15 +1,17 @@
 #include <iostream>
 using namespace std;
 
-int main()
-{
-	for (int i = 1; i < 10; i++)
-	{
-		for (int j = 1; j < 10; j++)
-		{
-			cout << j << "x" << i << "=" << j * i << '\t';
-		}
-		cout << '\n';
-	}
-	return 0;
+int main() {
+    for (int i = 1; i <= 9; i++) {
+        for (int j = 1; j <= 9; j++) {
+            cout << j << "x" << i << "=" << j*i;
+            
+            if (j == 9) {
+                cout << "\n";
+            } else {
+                cout << "\t";
+            }
+        }
+    }
+    return 0;
 }
