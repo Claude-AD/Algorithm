6b475cc37eb818cc20d0c4cce97d657f2bdff9aa
Junhyeok Yun <claudead777@gmail.com>
2021-02-09 09:54:51 +0900

    Update Ch.2-2.cpp

--- C++/명품 C++ programming/Chapter 2/Ch.2-2.cpp
+++ C++/명품 C++ programming/Chapter 2/Ch.2-2.cpp
@@ -2,11 +2,11 @@
 using namespace std;
 
 int main() {
-    for (int i = 1; i <= 9; i++) {
-        for (int j = 1; j <= 9; j++) {
+    for (int i = 1; i <= 9; i++) {  // 구구단 뒷 숫자
+        for (int j = 1; j <= 9; j++) { // 구구단 앞 숫자
             cout << j << "x" << i << "=" << j*i;
             
-            if (j == 9) {
+            if (j == 9) {   // 9 기준으로 줄 바꿈
                 cout << "\n";
             } else {
                 cout << "\t";
