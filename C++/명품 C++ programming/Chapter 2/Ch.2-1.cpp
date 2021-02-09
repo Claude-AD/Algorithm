e1dc35d0c48d3ae2b8065f3c2f9e46990dbdec05
Junhyeok Yun <claudead777@gmail.com>
2021-02-09 09:53:44 +0900

    Update Ch.2-1.cpp

--- C++/명품 C++ programming/Chapter 2/Ch.2-1.cpp
+++ C++/명품 C++ programming/Chapter 2/Ch.2-1.cpp
@@ -4,10 +4,10 @@ using namespace std;
 int main() {
     for (int i = 1; i <= 100; i++) {
         cout << i;
-        if (i % 10 == 0) {
+        if (i % 10 == 0) {  // 10단위로 끊어서 줄 바꿈
             cout << "\n";
         }
-        else {
+        else {              // 나머지는 tab으로 구분
             cout << "\t";
         }
     }
