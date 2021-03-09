bed5e34b79faf03977bb51e0b33040e7a30bfc0d
Junhyeok Yun <claudead777@gmail.com>
2021-03-09 13:27:16 +0900

    Update Ch.2-15

--- C++/명품 C++ programming/Chapter 2/Ch.2-15.cpp
+++ C++/명품 C++ programming/Chapter 2/Ch.2-15.cpp
@@ -1,41 +1,47 @@
 #include <iostream>
-#include <cstring>
 using namespace std;
 
-int main()
-{
-	while (true)
-	{
-		int num1, num2;
-		char _operator;
-		cout << "? ";
-		cin >> num1 >> _operator >> num2;
+void printResult(int num1, int num2, char oper, int result) {
+    cout << num1 << " " << oper << " " << num2 << " "
+    << "=" << " " << result << endl;
+}
 
-		int result;
-		switch (_operator)
-		{
-		case '+' :
-			result = num1 + num2;
-			break;
-
-		case '-' :
-			result = num1 - num2;
-			break;
-
-		case '*' :
-			result = num1 * num2;
-			break;
-
-		case '/' :
-			result = num1 / num2;
-			break;
-
-		case '%' :
-			result = num1 % num2;
-			break;
-		}
-
-		printf("%d %c %d = %d\n", num1, _operator, num2, result);
-	}
-	return 0;
+int main() {
+    while(1) {
+        int num1, num2;
+        char oper;
+        
+        cout << "? ";
+        cin >> num1 >> oper >> num2;
+        
+        int result = 0;
+        
+        switch (oper) {
+            case '+':
+                result = num1 + num2;
+                break;
+                
+            case '-':
+                result = num1 - num2;
+                break;
+                
+            case '*':
+                result = num1 * num2;
+                break;
+                
+            case '/':
+                result = num1 / num2;
+                break;
+                
+            case '%':
+                result = num1 % num2;
+                break;
+                
+            default:
+                cout << "Operator Error!\n";
+                break;
+        }
+        printResult(num1, num2, oper, result);
+    }
+    return 0;
 }
